fafe518b4b9af268a03782b12e17e2bb083a87a6
Sergey Yevtushok <ggazila@gmail.com>
2021-02-25 20:46:21 +0200

    7K task

--- codewars2021/src/test/java/task/seven/SampleTest6.java
+++ codewars2021/src/test/java/task/seven/SampleTest6.java
@@ -1,18 +1,21 @@
 package task.seven;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 import org.junit.runners.JUnit4;
 
 public class SampleTest6 {
     @Test
     public void testSomething() {
-        doTest("krishan", "nahsirk");
         doTest("ultr53o?n", "nortlu");
+        doTest("krishan", "nahsirk");
         doTest("ab23c", "cba");
         doTest("krish21an", "nahsirk");
     }
+
     private void doTest(final String str, final String expected) {
-        assertEquals(expected, Kata.reverseLetter(str));
+        assertEquals(expected, Kata6.reverseLetter(str));
     }
 }
