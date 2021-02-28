44cbafa86311b29a2c9da1f79cd9bcc9258e7ab9
Sergey Yevtushok <ggazila@gmail.com>
2021-02-28 19:34:13 +0200

    some mate react

---
+++ reactapp/src/Queue.test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Queue {
+  /**
+   * Add value to the queue
+   * @param { Number } value
+   *
+   * @returns { void }
+   */
+  enqueue(value) {
+    // write code here
+  }
+
+  /**
+   * Return queue size
+   * @returns { Number }
+   */
+  size() {
+    // write code here
+  }
+
+  /**
+   * Return the first added value
+   * @returns { Number | null }
+   */
+  peek() {
+    // write code here
+  }
+
+  /**
+   * Return the first added value and remove it from the queue
+   * @returns { Number | null }
+   */
+  dequeue() {
+    // write code here
+  }
+
+  /**
+   * Empty the queue
+   * @returns { void }
+   */
+  clear() {
+    // write code here
+  }
+
+  /**
+   * Serialize the queue
+   * @returns { Number[] }
+   */
+  serialize() {
+    // write code here
+  }
+}
